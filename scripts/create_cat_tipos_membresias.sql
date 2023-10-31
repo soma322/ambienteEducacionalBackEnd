3371040a57b88548868673ac8de913bc4aa70b36
soma322 <vampari58@gmail.com>
2023-10-31 13:36:05 -0700

    cambios en scripts

--- scripts/create_cat_tipos_membresias.sql
+++ scripts/create_cat_tipos_membresias.sql
@@ -1,5 +1,5 @@
 create table cat_tipos_membresias(
-id SERIAL PRIMARY KEY not null
+id SERIAL PRIMARY KEY not null,
 nombre varchar not null,
 activo boolean not null default true
 );
--- scripts/insert_tip.sql
+++ scripts/insert_tip.sql
@@ -8,17 +8,17 @@ insert into ctl_fuzzyantecedentes(consecuencia,membresias,nombre,rangomin,rangom
 
 
 
-insert into ctl_fuzzymembresia (consecuencia,antecedente,tipo, nombre, rangomin,rangomax, rangofinal )  values (1,0,1,'bajo',0,0,5);
-insert into ctl_fuzzymembresia (consecuencia,antecedente,tipo, nombre, rangomin,rangomax, rangofinal )  values (1,0,1,'medio',0,5,10);
-insert into ctl_fuzzymembresia (consecuencia,antecedente,tipo, nombre, rangomin,rangomax, rangofinal )  values (1,0,1,'alto',5,10,10);
+insert into ctl_fuzzymembresia (consecuencia,antecedente,tipo, nombre, rango1,rango2, rango3,rango4 )  values (1,0,1,'bajo',0,0,0,5);
+insert into ctl_fuzzymembresia (consecuencia,antecedente,tipo, nombre, rango1,rango2, rango3,rango4 )  values (1,0,1,'medio',0,0,5,10);
+insert into ctl_fuzzymembresia (consecuencia,antecedente,tipo, nombre, rango1,rango2, rango3,rango4 )  values (1,0,1,'alto',5,0,10,10);
 
-insert into ctl_fuzzymembresia (consecuencia,antecedente,tipo, nombre, rangomin,rangomax, rangofinal )  values (0,1,1,'pobre',0,0,5);
-insert into ctl_fuzzymembresia (consecuencia,antecedente,tipo, nombre, rangomin,rangomax, rangofinal )  values (0,1,1,'promedio',0,5,10);
-insert into ctl_fuzzymembresia (consecuencia,antecedente,tipo, nombre, rangomin,rangomax, rangofinal )  values (0,1,1,'bueno',5,10,10);
+insert into ctl_fuzzymembresia (consecuencia,antecedente,tipo, nombre, rango1,rango2, rango3,rango4 )  values (0,1,1,'pobre',0,0,0,5);
+insert into ctl_fuzzymembresia (consecuencia,antecedente,tipo, nombre, rango1,rango2, rango3,rango4 )  values (0,1,1,'promedio',0,0,5,10);
+insert into ctl_fuzzymembresia (consecuencia,antecedente,tipo, nombre, rango1,rango2, rango3,rango4 )  values (0,1,1,'bueno',5,0,10,10);
 
-insert into ctl_fuzzymembresia (consecuencia,antecedente,tipo, nombre, rangomin,rangomax, rangofinal )  values (0,2,1,'pobre',0,0,5);
-insert into ctl_fuzzymembresia (consecuencia,antecedente,tipo, nombre, rangomin,rangomax, rangofinal )  values (0,2,1,'promedio',0,5,10);
-insert into ctl_fuzzymembresia (consecuencia,antecedente,tipo, nombre, rangomin,rangomax, rangofinal )  values (0,2,1,'bueno',5,10,10);
+insert into ctl_fuzzymembresia (consecuencia,antecedente,tipo, nombre, rango1,rango2, rango3,rango4 )  values (0,2,1,'pobre',0,0,0,5);
+insert into ctl_fuzzymembresia (consecuencia,antecedente,tipo, nombre, rango1,rango2, rango3,rango4 )  values (0,2,1,'promedio',0,0,5,10);
+insert into ctl_fuzzymembresia (consecuencia,antecedente,tipo, nombre, rango1,rango2, rango3,rango4 )  values (0,2,1,'bueno',5,0,10,10);
 
 
 insert into cat_fuzzyrules (consecuenta_id,condiciones,consecuencia) values (1,'calidad pobre & servicio pobre & ambiente pobre', 'propina bajo');
