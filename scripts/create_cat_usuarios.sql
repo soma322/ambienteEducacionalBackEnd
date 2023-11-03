e9981e745211c620e900ec29d92d14f5a45f5d7d
soma322 <vampari58@gmail.com>
2023-11-03 06:31:40 -0700

    1

--- scripts/create_cat_usuarios.sql
+++ scripts/create_cat_usuarios.sql
@@ -8,3 +8,7 @@ fec_modificacion timestamp without time zone NOT NULL DEFAULT now(),
 opc_activo boolean NOT NULL DEFAULT true,
 CONSTRAINT cat_usuarios_pkey PRIMARY KEY (pk_cat_usuarios)
 );
+
+
+insert into cat_usuarios (nombre,apellido_paterno,apellido_materno) values ('Fernando','Vazquez','Cota')
+insert into cat_usuarios (nombre,apellido_paterno,apellido_materno) values ('admin','admin','admin')
